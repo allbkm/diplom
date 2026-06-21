7883034cf89a3b79350439733e1ab5f3ad1a753d
allbkm <91130798+allbkm@users.noreply.github.com>
2026-06-21 12:16:38 +0400

    Update urls.py
    ADDING ROUTES FOR SOCIAL AUTH

--- diplom_project/orders/urls.py
+++ diplom_project/orders/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),
+    path('auth/', include('social_django.urls', namespace='social')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
